67bca4be8382a1f41ec191fdbf9fb06e2bdea538
Mark Tyrkba <awfulmarco@gmail.com>
2025-08-07 18:51:27 +0300

    Close TODO(#24): Report error's with location in stalkr purging mode

--- src/loc.rs
+++ src/loc.rs
@@ -1,8 +1,9 @@
 use crate::fm::{self, FileId, FileManager};
 
 use std::fmt;
+use std::ops::Deref;
 
-#[derive(Debug)]
+#[derive(Copy, Clone, Debug)]
 pub struct Loc(pub FileId, pub u32);
 
 impl Loc {
@@ -13,22 +14,48 @@ impl Loc {
     #[doc(alias = "row")]
     pub const fn line_number(&self) -> u32 { self.1 }
 
-    #[allow(unused)]
     #[inline(always)]
-    pub fn display<'a>(&self, fm: &'a FileManager) -> DisplayLoc<'a> {
-        let file_path = fm.get_file_path_unchecked(self.0);
-        DisplayLoc { file_path, line_number: self.1 }
+    pub fn display_from_str<'a>(&self, file_path: &'a str) -> DisplayLoc<&'a str> {
+        DisplayLoc {
+            file_path: FilePathDisplay(file_path),
+            line_number: self.1
+        }
+    }
+
+    #[inline(always)]
+    pub fn display<'a>(&self, fm: &'a FileManager) -> DisplayLoc<fm::FilePathGuard<'a>> {
+        DisplayLoc {
+            file_path: FilePathDisplay(fm.get_file_path_unchecked(self.0)),
+            line_number: self.1
+        }
     }
 }
 
-pub struct DisplayLoc<'a> {
-    file_path: fm::FilePathGuard<'a>,
-    line_number: u32
+// wrapper that enables generic display for both `&str` and any other refs
+pub struct FilePathDisplay<T: Deref>(pub T);
+
+impl<T: Deref> fmt::Display for FilePathDisplay<T>
+where
+    T::Target: AsRef<str>
+{
+    #[inline(always)]
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str(self.0.as_ref())
+    }
 }
 
-impl fmt::Display for DisplayLoc<'_> {
-    fn fmt(&self, fm: &mut fmt::Formatter<'_>) -> fmt::Result {
+pub struct DisplayLoc<T: Deref> {
+    pub file_path: FilePathDisplay<T>,
+    pub line_number: u32
+}
+
+impl<T: Deref> fmt::Display for DisplayLoc<T>
+where
+    T::Target: AsRef<str>
+{
+    #[inline(always)]
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let Self { file_path, line_number: row } = self;
-        write!(fm, "{file_path}:{row}")
+        write!(f, "{file_path}:{row}")
     }
 }
