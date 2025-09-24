ae9a018860fbaceab907713e4877e6e7495060d6
Mark Tyrkba <awfulmarco@gmail.com>
2025-09-24 16:28:38 +0300

    Remove closed TODO(#35): Have determined comment-type per TODO

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,3 @@
-// TODO(#35): Have determined comment-type per TODO
-// e.g. this:
-// ```
-//  // TODO: Generalize `diag` param type in not(fancy-diagnostics)
-//  // in DiagnosticRenderer::render_to_string
-//  #[inline]
-//  #[cfg(not(feature = "fancy-diagnostics"))]
-// ```
-// won't work correctly and #[inline] and #[cfg(..)] lines will
-// be recognized as continuation of the description.
-
 // TODO(#32): Implement closing mode
 
 use stalkr::cli::Cli;
