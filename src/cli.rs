43c4e0fb2e3e9e6636e624839fd730f32f86a670
Mark Tyrkba <awfulmarco@gmail.com>
2025-11-29 19:47:17 +0300

    chore: update version in src/cli.rs

--- src/cli.rs
+++ src/cli.rs
@@ -8,7 +8,7 @@ use clap::{Parser, Subcommand};
 #[clap(
     name = "stalkr",
     about = "stalkr: multi-threaded TODO reporter",
-    version = "0.1.1",
+    version = "0.1.2",
     override_usage = "stalkr [SUBCOMMAND] [OPTIONS]"
 )]
 pub struct Cli {
